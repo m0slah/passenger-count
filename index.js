d49af8b8a547eca7214eb8524e1df8589b3ed40c
m0slah <muhammedslahqadir@gmail.com>
2024-06-27 09:23:57 +0300

    write the first variable

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-document.getElementById("passenger-count").innerHTML=6;
+// document.getElementById("count").innerHTML=6;
+
+let count=0;
+
+console.log(count);
+
+console.log(myAge) //ReferenceError: Cannot access 'myAge' before initialization
+
+let myAge=20;
+
+console.log(myAge)
