22cfe610c9319cfabef020f3afe89f8e2b0f6618
m0slah <muhammedslahqadir@gmail.com>
2024-06-30 08:58:21 +0300

    reassigning and incrementing

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 // document.getElementById("count").innerHTML=6;
 
-// let firstBatch=5;
-// let secondBatch=7;
-// let count=firstBatch+ secondBatch;
-// console.log(count);
+// let count=4;
+// count=3;
+// count=2;
+// console.log(count)
 
-let myAge=23;
-let humanDogRatio =7;
-let myDogAge=myAge * humanDogRatio;
-console.log(myDogAge)
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
+
+
+
 
