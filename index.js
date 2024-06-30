7760ed7d6a2547ff3941f2fd1e14e4839b10482f
m0slah <muhammedslahqadir@gmail.com>
2024-06-30 09:17:38 +0300

    the onclick event listener

--- index.js
+++ index.js
@@ -5,15 +5,6 @@
 // count=2;
 // console.log(count)
 
-// let bonusPoints = 50
-// console.log(bonusPoints)
-//
-// bonusPoints = bonusPoints + 50
-// console.log(bonusPoints)
-//
-// bonusPoints = bonusPoints - 75
-// console.log(bonusPoints)
-//
-// bonusPoints = bonusPoints + 45
-// console.log(bonusPoints)
-
+const increment=()=>{
+    console.log("button was clicked")
+}
