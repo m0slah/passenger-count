5dc9967079d55d8b2eea3ee2b9ceacb814e1dab2
m0slah <muhammedslahqadir@gmail.com>
2024-06-30 09:11:09 +0300

    add a button

--- index.js
+++ index.js
@@ -5,18 +5,15 @@
 // count=2;
 // console.log(count)
 
-let bonusPoints = 50
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
-
-
-
+// let bonusPoints = 50
+// console.log(bonusPoints)
+//
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+//
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+//
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
 
